781f3a8186645fb8aee164d0cdf5060ab59cf2dd
Geert Barentsen <geert@barentsen.be>
2014-02-17 16:08:16 +0000

    Initial commit

---
+++ boilerplate.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""[one-line description]"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+__author__ = "Geert Barentsen"
+__copyright__ = "Copyright, The Authors"
+__credits__ = []
+
+import logging
+log = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    pass
